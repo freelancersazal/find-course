06e74c5bbd87eba4e59d34f6e200a2a7ff5526ac
freelancersazal <freelancersazal@gmail.com>
2022-11-26 19:20:22 +0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+
+    "use strict";
+
+
+    //=====NICE SELECT=====
+    $('#select_js').niceSelect();
+    $('#select_js2').niceSelect();
+    $('#select_js3').niceSelect();
+    $('#select_js4').niceSelect();
+    $('#select_js5').niceSelect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
